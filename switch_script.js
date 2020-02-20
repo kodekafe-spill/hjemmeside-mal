1ddee06b66fcb8dcfd2b57830867f14483a402f1
maudstue <maudstue@users.noreply.github.com>
2020-02-20 10:29:35 +0100

    gjorde de andre html sidene om til div-er på hovedsiden som gjemmes eller vises, så det ikke blir så mange filer. La til light-versjon og bytte mellom dem

---
+++ switch_script.js
@@ -0,0 +1,49 @@
+
+    function hjem() {
+        var home = document.getElementById("hjem");
+        var about = document.getElementById("om_oss");
+        var what = document.getElementById("hva_gjor_vi");
+        var project = document.getElementById("prosjekter");
+        home.style.display = "block";
+        about.style.display = "none";
+        what.style.display = "none";
+        project.style.display = "none";
+    }
+    
+    function om_oss() {
+        var home = document.getElementById("hjem");
+        var about = document.getElementById("om_oss");
+        var what = document.getElementById("hva_gjor_vi");
+        var project = document.getElementById("prosjekter");
+        about.style.display = "block";
+        home.style.display = "none";
+        what.style.display = "none";
+        project.style.display = "none";
+    }
+    
+    function hva_gjor_vi() {
+        var home = document.getElementById("hjem");
+        var about = document.getElementById("om_oss");
+        var what = document.getElementById("hva_gjor_vi");
+        var project = document.getElementById("prosjekter");
+        what.style.display = "block";
+        about.style.display = "none";
+        home.style.display = "none";
+        project.style.display = "none";
+    }
+    
+    function prosjekter() {
+        var home = document.getElementById("hjem");
+        var about = document.getElementById("om_oss");
+        var what = document.getElementById("hva_gjor_vi");
+        var project = document.getElementById("prosjekter");
+        project.style.display = "block";
+        about.style.display = "none";
+        what.style.display = "none";
+        home.style.display = "none";
+    }
+    
+    document.addEventListener('DOMContentLoaded', function(){
+        hjem();
+        });
+
